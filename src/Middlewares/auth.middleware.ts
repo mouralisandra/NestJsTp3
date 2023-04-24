fd21a8a01571c58ca9f82cca84950fbfdd238fda
Sandra Mourali <sandra.mourali@insat.ucar.tn>
2023-04-24 16:59:11 +0100

    Solved Authentification and pagination issues

--- src/Middlewares/auth.middleware.ts
+++ src/Middlewares/auth.middleware.ts
@@ -7,16 +7,16 @@ export class AuthMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
     const token = req.headers['auth-user']; 
     next();
-    // if (token) {
-    //   try {
-    //     const decoded = verify(token, 'your-secret-key'); 
-    //     req['userId'] = decoded['userId']; 
-    //     next();
-    //   } catch (err) {
-    //     res.json({ message: 'Token invalide' }); 
-    //   }
-    // } else {
-    //   res.json({ message: 'Accès non autorisé' }); 
-    // }
+    if (token) {
+      try {
+        const decoded = verify(token, 'your-secret-key'); 
+        req['userId'] = decoded['userId']; 
+        next();
+      } catch (err) {
+        res.json({ message: 'Token invalide' }); 
+      }
+    } else {
+      res.json({ message: 'Accès non autorisé' }); 
+    }
   }
 }
